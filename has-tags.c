5b3534fe751057c602d2e3caf8968c8d2da2c2de
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-10-24 00:50:26 -0700

    Gobble memory like there's no tomorrow

--- has-tags.c
+++ has-tags.c
@@ -4,6 +4,19 @@
 static const char * const TAGS_XATTR_NAME = "com.apple.metadata:_kMDItemUserTags";
 static const int TAGS_XATTR_OPTIONS = XATTR_NOFOLLOW;
 
+static void *allocate(CFIndex size, CFOptionFlags hint, void *info) {
+	return malloc((size_t)size * 2);
+}
+
+static CFIndex preferredSize(CFIndex size, CFOptionFlags hint, void *info) {
+	return size * 2;
+}
+
+static void *reallocate(void *ptr, CFIndex newSize, CFOptionFlags hint, void *info) {
+	if (ptr == NULL || newSize <= 0) return NULL;
+	return realloc(ptr, (size_t)newSize * 2);
+}
+
 int main (int argc, const char **argv) {
 	if (argc < 3) {
 		fprintf(stderr, "Usage: has-tags path tag [tag ...]");
@@ -26,10 +39,18 @@ int main (int argc, const char **argv) {
 		if (bufferLength <= 0) return EXIT_FAILURE;
 	} while (bufferLength > originalLength);
 
-	CFDataRef data = CFDataCreateWithBytesNoCopy(NULL, buffer, bufferLength, kCFAllocatorNull);
+	CFAllocatorRef allocator = CFAllocatorCreate(kCFAllocatorUseContext, &(CFAllocatorContext){
+		.version = 0,
+		.allocate = &allocate,
+		.reallocate = &reallocate,
+		.deallocate = NULL,
+		.preferredSize = &preferredSize
+	});
+
+	CFDataRef data = CFDataCreateWithBytesNoCopy(allocator, buffer, bufferLength, kCFAllocatorNull);
 
 	CFErrorRef error = nil;
-	CFArrayRef rawTags = CFPropertyListCreateWithData(NULL, data, kCFPropertyListImmutable, NULL, &error);
+	CFArrayRef rawTags = CFPropertyListCreateWithData(allocator, data, kCFPropertyListImmutable, NULL, &error);
 	if (rawTags == NULL) {
 		fprintf(stderr, "Could not parse tags from xattr: %s", CFStringGetCStringPtr(CFCopyDescription(error), kCFStringEncodingUTF8));
 		return EXIT_FAILURE;
